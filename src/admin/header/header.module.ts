0e35a361c15e451ffc1b644bf82a61c604221919
15138730370 <1037483576@qq.com>
2017-10-02 17:02:42 +0800

    header component

---
+++ src/admin/header/header.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { AdminHeader } from './header';
+@NgModule({
+    imports: [],
+    exports: [
+        AdminHeader
+    ],
+    declarations: [
+        AdminHeader
+    ]
+})
+export class AdminHeaderModule{}
---
+++ src/admin/header/header.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'admin-header',
+    templateUrl: './header.html',
+    styleUrls: ['./header.scss']
+})
+export class AdminHeader implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
