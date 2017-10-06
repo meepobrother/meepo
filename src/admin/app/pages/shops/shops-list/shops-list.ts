b105229fa34f1c26f6cc5a4a18faa640c600c057
15138730370 <1037483576@qq.com>
2017-10-06 13:14:52 +0800

    主题页面管理

--- src/admin/app/pages/shops/shops-list/shops-list.ts
+++ src/admin/app/pages/shops/shops-list/shops-list.ts
@@ -12,4 +12,6 @@ export class ShopsList implements OnInit {
     ) { }
 
     ngOnInit() { }
+
+    add() { }
 }
--- src/admin/app/pages/themes/themes-page.module.ts
+++ src/admin/app/pages/themes/themes-page.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemesPage } from './themes-page';
+import { ThemesListFree } from './themes-list-free';
+import { ShareModule } from '../../share';
 import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
     {
@@ -10,12 +12,14 @@ const routes: Routes = [
 ];
 @NgModule({
     declarations: [
-        ThemesPage
+        ThemesPage,
+        ThemesListFree
     ],
-    imports: [ CommonModule, RouterModule.forChild(routes) ],
+    imports: [CommonModule, RouterModule.forChild(routes), ShareModule],
     exports: [
-        ThemesPage
+        ThemesPage,
+        ThemesListFree
     ],
     providers: [],
 })
-export class ThemesPageModule {}
+export class ThemesPageModule { }
