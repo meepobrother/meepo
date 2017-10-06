49c57efb2d52129716a4da2f50721e46e8502add
15138730370 <1037483576@qq.com>
2017-10-06 13:25:52 +0800

    主题皮肤

--- src/admin/app/pages/themes/themes-page.module.ts
+++ src/admin/app/pages/themes/themes-page.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemesPage } from './themes-page';
+
 import { ThemesListFree } from './themes-list-free';
+import { ThemesMine } from './themes-mine';
+import { ThemesShops } from './themes-shops';
+
+
 import { ShareModule } from '../../share';
 import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
@@ -13,12 +18,16 @@ const routes: Routes = [
 @NgModule({
     declarations: [
         ThemesPage,
-        ThemesListFree
+        ThemesListFree,
+        ThemesMine,
+        ThemesShops
     ],
     imports: [CommonModule, RouterModule.forChild(routes), ShareModule],
     exports: [
         ThemesPage,
-        ThemesListFree
+        ThemesListFree,
+        ThemesMine,
+        ThemesShops
     ],
     providers: [],
 })
