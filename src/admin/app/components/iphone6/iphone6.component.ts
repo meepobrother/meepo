264dc95dfbcaaa0e945b7dd17e2406c7f18f7726
15138730370 <1037483576@qq.com>
2017-10-07 17:17:05 +0800

    设备预览

--- src/admin/app/components/iphone6/iphone6.component.ts
+++ src/admin/app/components/iphone6/iphone6.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, Renderer2} from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'iphone6',
@@ -6,16 +6,19 @@ import {Component, ElementRef, OnInit, Renderer2} from '@angular/core';
   styleUrls: ['./iphone6.component.scss']
 })
 export class Iphone6Component implements OnInit {
+  _background: string = 'black';
+  _open: boolean = false;
+  constructor() { }
 
-  constructor(
-      private ele: ElementRef,
-      private render: Renderer2
-  ) {
-    this.render.setStyle(this.ele.nativeElement,'display','inline-block')
+  ngOnInit() { }
 
+  open() {
+    this._open = !this._open;
+    if (this._open) {
+      this._background = 'white';
+    } else {
+      this._background = 'black';
+    }
   }
-
-  ngOnInit() {
-  }
-
 }
+
