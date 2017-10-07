035f39eac5ec2b74f4dce71cbdb80ce307fa5948
15138730370 <1037483576@qq.com>
2017-10-07 11:50:21 +0800

    编辑器

--- src/admin/app/components/editor/editor.ts
+++ src/admin/app/components/editor/editor.ts
@@ -1,12 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 @Component({
     selector: 'editor',
     templateUrl: './editor.html',
-    styleUrls: ['./editor.scss']
+    styleUrls: ['./editor.scss'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => EditorComponent),
+            multi: true
+        }
+    ]
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent implements OnInit, ControlValueAccessor {
+    onChangeFn: (_: any) => {};
+    
     constructor() { }
 
     ngOnInit() { }
+
+    writeValue(obj: any): void {
+        
+    }
+    
+    registerOnChange(fn: any): void {
+        this.onChangeFn = fn;
+    }
+    registerOnTouched(): void {
+
+    }
 }
