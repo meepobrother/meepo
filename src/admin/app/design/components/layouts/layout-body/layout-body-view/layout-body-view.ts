ae6ef07d88e4f18249ecb8f1f03564762dbd681d
15138730370 <1037483576@qq.com>
2017-10-09 00:13:57 +0800

    布局

---
+++ src/admin/app/design/components/layouts/layout-body/layout-body-view/layout-body-view.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'layout-body-view',
+    templateUrl: './layout-body-view.html',
+    styleUrls: ['./layout-body-view.scss']
+})
+export class LayoutBodyView implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
