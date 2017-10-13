931e572f19f7943a32e7268df178409b41c789c0
15138730370 <1037483576@qq.com>
2017-10-13 16:27:44 +0800

    保存

---
+++ src/mobile-new/components/meepo-advs/meepo-advs.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var html = require('./meepo-advs.html');
+console.log(html);
+var MeepoAdvs = /** @class */ (function () {
+    function MeepoAdvs() {
+        this.templateStr = html;
+    }
+    return MeepoAdvs;
+}());
+exports.MeepoAdvs = MeepoAdvs;
---
+++ src/mobile-new/index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var meepo_advs_1 = require("./components/meepo-advs/meepo-advs");
+var COMPONENTS = {
+    'meepo-advs': new meepo_advs_1.MeepoAdvs()
+};
