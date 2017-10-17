b79a91b50062d5585ff1f1121be9eca16b56ae71
15138730370 <1037483576@qq.com>
2017-10-17 08:46:04 +0800

    保存一下

--- src/admin/app/pages/goods/goods-tags/goods-tags.module.ts
+++ src/admin/app/pages/goods/goods-tags/goods-tags.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GoodsTags } from './goods-tags';
 import { GoodsTagsAdd } from './goods-tags-add';
-
+import { ShareModule } from '../../../share';
 @NgModule({
     declarations: [
         GoodsTags,
         GoodsTagsAdd
     ],
-    imports: [ CommonModule ],
+    imports: [ CommonModule, ShareModule ],
     exports: [
         GoodsTags,
         GoodsTagsAdd
--- src/admin/app/pages/goods/goods-tags/goods-tags.ts
+++ src/admin/app/pages/goods/goods-tags/goods-tags.ts
@@ -1,12 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatDialog } from '@angular/material';
+import { GoodsTagsAdd } from './goods-tags-add';
+import { ApiService } from '../../../core';
 @Component({
     selector: 'goods-tags',
     templateUrl: './goods-tags.html',
     styleUrls: ['./goods-tags.scss']
 })
 export class GoodsTags implements OnInit {
-    constructor() { }
+    list: any[] = [];
+    constructor(
+        public dialog: MatDialog,
+        public api: ApiService
+    ) { }
 
-    ngOnInit() { }
+    ngOnInit() { 
+        this.getList();
+    }
+
+    getList() {
+        this.api.mpost('goods.getListGoodsTags', { page: 1, psize: 30 }).subscribe((res: any) => {
+            this.list = res.info;
+        });
+    }
+
+    add() {
+        const dialogRef = this.dialog.open(GoodsTagsAdd);
+        dialogRef.afterClosed().subscribe(res => {
+            if (res) {
+                this.api.mpost('goods.addGoodsTags', res).subscribe((res: any) => {
+                    this.list.unshift(res.info);
+                });
+            }
+        });
+    }
+
+    edit(item: any){
+        const dialogRef = this.dialog.open(GoodsTagsAdd,{ data: item});
+        dialogRef.afterClosed().subscribe(res => {
+            if (res) {
+                this.api.mpost('goods.editGoodsTags', res).subscribe((res: any) => {
+                    this.getList();
+                });
+            }
+        });
+    }
 }
