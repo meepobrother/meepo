204260149ef95e186eac1bb22c5e75b92e9a35f6
15138730370 <1037483576@qq.com>
2017-11-04 13:24:02 +0800

    save

--- src/admin/app/components/public_api.ts
+++ src/admin/app/components/public_api.ts
@@ -6,7 +6,6 @@ export * from './text-adv';
 export * from './price-list';
 export * from './overlay';
 export * from './cover';
-export * from './dnd';
 export * from './color-select';
 export * from '../design/iphone6';
 export * from './menu';
@@ -15,7 +14,5 @@ export * from './weui-picker';
 
 export * from './editor';
 export * from './dialog';
-export * from './location-picker';
 
-export * from './uploader';
 
