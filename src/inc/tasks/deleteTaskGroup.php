76bb7ee3a95e1c926fadfeac906ed170fb6be997
15138730370 <1037483576@qq.com>
2017-11-09 14:10:37 +0800

    删除任务组

--- src/inc/tasks/deleteTaskGroup.php
+++ src/inc/tasks/deleteTaskGroup.php
@@ -2,5 +2,6 @@
 global $_W,$_GPC;
 $input = $this->__input['encrypted'];
 
+pdo_delete('imeepos_runner4_tasks_group',array('id'=>$input['id']));
 $this->info = $input;
 return $this;
--- src/inc/tasks/editTaskGroup.php
+++ src/inc/tasks/editTaskGroup.php
@@ -7,13 +7,15 @@ $group['uniacid'] = $_W['uniacid'];
 $group['title'] = $input['title'];
 $group['displayorder'] = $input['displayorder'];
 $group['setting'] = serialize($group['setting']);
-
-if(!empty($input['id'])){
-    pdo_update('imeepos_runner4_tasks_group',$group,array('id'=>$input['id']));
-    $group['id'] = $input['id'];
-}else{
-    pdo_insert('imeepos_runner4_tasks_group',$group);
-    $group['id'] = pdo_insertid();
+if(!empty($group['title'])){
+    if(!empty($input['id'])){
+        pdo_update('imeepos_runner4_tasks_group',$group,array('id'=>$input['id']));
+        $group['id'] = $input['id'];
+    }else{
+        pdo_insert('imeepos_runner4_tasks_group',$group);
+        $group['id'] = pdo_insertid();
+    }
 }
+
 $this->info = $group;
 return $this;
