2b07091734ea5c2799437fe456937e94379eddfc
15138730370 <1037483576@qq.com>
2017-11-11 17:43:22 +0800

    保存

--- src/admin/app/design/components/runner/runner-register/runner-register-setting/runner-register-setting.ts
+++ src/admin/app/design/components/runner/runner-register/runner-register-setting/runner-register-setting.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RunnerRegisterDefault } from '../../../../classes';
 import { ApiService } from '../../../../../core';
+declare const layui: any;
+declare const jQuery: any;
+
 @Component({
     selector: 'runner-register-setting',
     templateUrl: './runner-register-setting.html',
@@ -8,10 +11,35 @@ import { ApiService } from '../../../../../core';
 })
 export class RunnerRegisterSetting implements OnInit {
     @Input() widget: RunnerRegisterDefault = new RunnerRegisterDefault();
-    
+    item: any;
+    index: number;
     constructor(
         public api: ApiService
     ) { }
 
-    ngOnInit() { }
+    ngOnInit() { 
+        layui.use('upload', () => {
+            var upload = layui.upload;
+            //执行实例
+            var uploadInst = upload.render({
+                elem: jQuery('.uploader') //绑定元素
+                , url: this.api.doMobileUrl('upload','imeepos_runner')//this.api.murl('utility/file/upload', { type: 'image' }) //上传接口
+                , done: (res, index, upload) => {
+                    //上传完毕回调
+                    // this.widget.image = res.url;
+                    this.item['src'] = res.url;
+                    this.widget.images[this.index] = this.item;
+                }
+                , error: () => {
+                    //请求异常回调
+                    console.log('上传失败');
+                }
+            });
+        });
+    }
+
+    upload(item: any, index: number){
+        this.item = item;
+        this.index = index;
+    }
 }
