96711b42ec9894683965bb58600a00a7b386af73
15138730370 <1037483576@qq.com>
2017-11-15 15:51:28 +0800

    保存

--- src/admin/app/pages/skills/skills-group/skills-group-add/skills-group-add.ts
+++ src/admin/app/pages/skills/skills-group/skills-group-add/skills-group-add.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'skills-group-add',
@@ -6,7 +8,34 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./skills-group-add.scss']
 })
 export class SkillsGroupAdd implements OnInit {
-    constructor() { }
+    form: FormGroup;
+    constructor(
+        @Inject(MAT_DIALOG_DATA) public data: any,
+        public dialog: MatDialogRef<any>,
+        public fb: FormBuilder
+    ) {
+        this.form = this.fb.group({
+            title: [''],
+            id: [''],
+            uniacid: ['']
+        });
 
+        this.dialog.afterOpen().subscribe(res=>{
+            let { title , id, uniacid} = this.data;
+            this.form.get('title').setValue(title);
+            this.form.get('id').setValue(id);
+            this.form.get('uniacid').setValue(uniacid);
+        });
+    }
     ngOnInit() { }
+    save(){
+        this.dialog.close(this.form.value);
+    }
+    cancel(){
+        this.dialog.close();
+    }
+
+    close(){
+        this.cancel();
+    }
 }
