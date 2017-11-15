aeb023bbd635c1f60a53fd4f47e5cd9a025c94da
15138730370 <1037483576@qq.com>
2017-11-15 12:11:59 +0800

    活动无限极分类

--- src/admin/app/pages/actives/actives-group/actives-group.ts
+++ src/admin/app/pages/actives/actives-group/actives-group.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from '../../../core';
 @Component({
     selector: 'actives-group',
     templateUrl: './actives-group.html',
     styleUrls: ['./actives-group.scss']
 })
 export class ActivesGroup implements OnInit {
-    constructor() { }
+    list: any[] = [];
+    constructor(
 
-    ngOnInit() {}
+        public api: ApiService
+    ) { }
+
+    ngOnInit() {
+        this.getList();
+    }
+
+    add(){}
+
+    getList(){
+        this.api.mpost('actives.getListActivesGroup',{}).subscribe((res: any)=>{
+            this.list = res.info;
+        })
+    }
 }
