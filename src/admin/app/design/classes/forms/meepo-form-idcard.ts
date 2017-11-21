54f9a1346c98a287acc76e7efd17c7b401439d85
15138730370 <1037483576@qq.com>
2017-11-21 11:40:30 +0800

    修复

--- src/admin/app/design/classes/forms/meepo-form-idcard.ts
+++ src/admin/app/design/classes/forms/meepo-form-idcard.ts
@@ -5,8 +5,8 @@ import { Widget } from '../widget';
 export class MeepoFormIdcardDefault extends Widget{
     show: boolean = true;
 
-    img1: string = 'https://meepo.com.cn/meepo/image/card-img1.png';
-    img2: string = 'https://meepo.com.cn/meepo/image/card-img2.png';
+    img1: string = 'https://meepo.com.cn/meepo/images/card-img1.png';
+    img2: string = 'https://meepo.com.cn/meepo/images/card-img2.png';
 
     constructor(){
         super();
